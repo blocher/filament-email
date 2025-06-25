f5596fadc2ce61e79db27da1410d52f4803473b0
Benjamin Locher <blocher@gmail.com>
2025-06-24 23:17:05 -0400

    fixing template

--- resources/views/html_view.blade.php
+++ resources/views/html_view.blade.php
@@ -1,5 +1,14 @@
-<x-filament-forms::field-wrapper :id="$this->getId()" :label="" :label-sr-only="$this->isLabelHidden()" :helper-text="$this->getHelperText()" :hint="$this->getHint()" :hint-icon="$this->getHintIcon()" :required="$this->isRequired()" :state-path="$this->getStatePath()">
+<x-filament-forms::field-wrapper
+    :id="$this->getId()"
+    label=""
+    :label-sr-only="$this->isLabelHidden()"
+    :helper-text="$this->getHelperText()"
+    :hint="$this->getHint()"
+    :hint-icon="$this->getHintIcon()"
+    :required="$this->isRequired()"
+    :state-path="$this->getStatePath()"
+>
     <div>
-        <iframe style="width: 100%; height:75vh;" srcdoc=" {{$getState() }}" seamless frameborder="0"></iframe>
+        <iframe style="width: 100%; height:75vh;" srcdoc="{{ $this->getState() }}" seamless frameborder="0"></iframe>
     </div>
 </x-filament-forms::field-wrapper>
