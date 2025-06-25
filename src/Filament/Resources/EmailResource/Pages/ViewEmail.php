2de8ec93ca87a5d8902b38a6a3f47145681fff7e
Benjamin Locher <blocher@gmail.com>
2025-06-24 23:29:39 -0400

    new template

--- src/Filament/Resources/EmailResource/Pages/ViewEmail.php
+++ src/Filament/Resources/EmailResource/Pages/ViewEmail.php
@@ -55,10 +55,10 @@ class ViewEmail extends ViewRecord
         ];
     }
 
-    public function render(): View
-    {
-        return view('filament-email::html_view', [
-            'email' => $this->record,
-        ])->layout(Filament::getCurrentPanel()?->getLayout());
-    }
+    // public function render(): View
+    // {
+    //     return view('filament-email::html_view', [
+    //         'email' => $this->record,
+    //     ])->layout(Filament::getCurrentPanel()?->getLayout());
+    // }
 }
